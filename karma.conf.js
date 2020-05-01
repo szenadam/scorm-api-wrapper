397cb42485a12e8d446bd7c258e1aeb74de6f21a
szenadam <szenadam1@gmail.com>
2020-05-01 14:35:00 +0200

    Add karma packages and karma conf

---
+++ karma.conf.js
@@ -0,0 +1,11 @@
+module.exports = function (config) {
+  config.set({
+    frameworks: ["jasmine", "karma-typescript"],
+    files: ["src/**/*.ts", "test/**/*.ts"],
+    preprocessors: {
+      "**/*.ts": "karma-typescript",
+    },
+    reporters: ["progress", "karma-typescript"],
+    browsers: ["Chrome"],
+  });
+};
